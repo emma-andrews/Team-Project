adb95184b55491c150ff47e9dac99edf643e4725
Emma Andrews <eandrews258@gmail.com>
2018-04-10 16:42:17 -0400

    Added information comment in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 #include <SFML/Graphics.hpp>
 #include "Engine.h"
 
+/* Welcome to our platformer!
+There will be multiple .cpp and .h files with this project, so make sure you are working in the correct one
+Engine.cpp defines all the game rendering and game time processing that is done
+Player.cpp defines the player character and its attributes, but input for it is located in ENGINE.CPP
+Levels.cpp defines all the map based items, including the imported tile sets Emma will be making
+Enemies.cpp defines the enemy movements, which requires AI components, so pathing towards the player, etc.
+If you have any questions about SFML, you can find the documentation here: https://www.sfml-dev.org/documentation/2.4.2/index.php
+Additionally, there are many resources out there for learning how to code a 2D platformer, and even if they do not use SFML, it will still be similar
+Please remember to push early and often! We do not want any lost work (if you want, you can create your own branch for what you are working on)*/
+
 int main() {
     Engine engine;
     engine.start();
